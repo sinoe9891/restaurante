f24e3d6533e991b95d7f29a6c2616f7a6bda7e7e
Zeeshan Ahmad <zeeshan.ahmad@sap.com>
2022-09-11 22:57:38 -0600

    merge

--- inc/templates/navbar-admin.php
+++ inc/templates/navbar-admin.php
@@ -1,3 +1,11 @@
+<?php
+$archivo = basename($_SERVER['PHP_SELF']);
+$actualidad = str_replace(".php", "", $archivo);
+?>
+
+</style>
+
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
 	<a href="" class="navbar-brand p-0">
 		<!-- <h1 class="text-primary m-0"><i class="fa fa-utensils me-3"></i>Restoran</h1> -->
@@ -8,10 +16,10 @@
 	</button>
 	<div class="collapse navbar-collapse" id="navbarCollapse">
 		<div class="navbar-nav ms-auto py-0 pe-4">
-			<a href="." class="nav-item nav-link active">Inicio</a>
-			<a href="menu" class="nav-item nav-link">Usuarios</a>
-			<a href="service" class="nav-item nav-link">Recetas</a>
-			<a href="about" class="nav-item nav-link">Menú</a>
+			<a href="dashboard" class="nav-item nav-link <?php echo $actualidad == 'dashboard' ? 'active' : 'false' ?>">Inicio</a>
+			<a href="usuarios" class="nav-item nav-link <?php echo $actualidad == 'usuarios' ? 'active' : '' ?>">Usuarios</a>
+			<a href="recetas-admin" class="nav-item nav-link <?php echo $actualidad == 'recetas-admin' ? 'active' : '' ?>">Recetas</a>
+			<a href="menu-admin" class="nav-item nav-link <?php echo $actualidad == 'menu-admin' ? 'active' : '' ?>">Menú</a>
 			<div class="nav-item dropdown">
 				<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Analytics</a>
 				<div class="dropdown-menu m-0">
